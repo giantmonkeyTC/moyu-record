96fc8d145246c7a330f5da8424fdb490d853c01b
gigakuma <qiang.l.x@gmail.com>
2020-03-20 18:34:09 +0800

    handler example

---
+++ app/src/main/java/cn/troph/tomon/core/network/socket/handlers/GuildCreate.kt
@@ -0,0 +1,9 @@
+package cn.troph.tomon.core.network.socket.handlers
+
+import cn.troph.tomon.core.Client
+import cn.troph.tomon.core.JsonData
+import cn.troph.tomon.core.network.socket.Handler
+
+val handleGuildCreate: Handler = { client: Client, packet: JsonData -> {
+
+}}
