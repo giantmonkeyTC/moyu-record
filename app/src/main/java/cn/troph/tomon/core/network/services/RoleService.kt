43ccaf5ef61add5d3d1f04a0b703a5ccf14cc4f7
giantmonkeyTC <1145092716@qq.com>
2020-03-24 15:54:51 +0800

    Role Service

--- app/src/main/java/cn/troph/tomon/core/network/services/RoleService.kt
+++ app/src/main/java/cn/troph/tomon/core/network/services/RoleService.kt
@@ -6,4 +6,40 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface RoleService {
+    @GET("guilds/{guildId}/roles")
+    fun getRoles(@Path("guildId") guildId: String, @Header("Authorization") token: String): Call<JsonArray>;
+
+    data class CreateRoleRequest(
+        val name: String,
+        val color: Int?,
+        val permissions: Int?
+    )
+
+    @POST("guilds/{guildId}/roles/{id}")
+    fun createRole(
+        @Path("guildId") guildId: String,
+        @Body request: CreateRoleRequest,
+        @Header("Authorization") token: String
+    ): Call<JsonData>
+
+    data class UpdateRoleRequest(
+        val name: String?,
+        val color: Int?,
+        val hoist: Boolean?,
+        val permissions: Int?
+    )
+
+    @PATCH("guilds/{guildId}/roles/{roleId}")
+    fun updateRole(
+        @Path("guildId") guildId: String, @Path("roleId") roleId: String, @Body request: UpdateRoleRequest, @Header(
+            "Authorization"
+        ) token: String
+    ): Call<JsonData>
+
+    @DELETE("guilds/{guildId}/roles/{roleId}")
+    fun deleteRole(
+        @Path("guildId") guildId: String, @Path("roleId") roleId: String, @Header(
+            "Authorization"
+        ) token: String
+    ): Call<JsonData>
 }
