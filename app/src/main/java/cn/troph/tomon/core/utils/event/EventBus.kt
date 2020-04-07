a4d8d3b5a651b44211375205c72d207ef7df72d6
gigakuma <qiang.l.x@gmail.com>
2020-04-08 03:36:19 +0800

    event bus

---
+++ app/src/main/java/cn/troph/tomon/core/utils/event/EventBus.kt
@@ -0,0 +1,21 @@
+package cn.troph.tomon.core.utils.event
+
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.kotlin.ofType
+
+interface EventBus {
+
+    fun postEvent(event: Any)
+
+    fun observeEvents(): Flowable<Any>
+
+    fun observeEventsOnUi(): Flowable<Any>
+}
+
+inline fun <reified T : Any> EventBus.observeEvent(): Flowable<T> {
+    return observeEvents().ofType()
+}
+
+inline fun <reified T : Any> EventBus.observeEventOnUi(): Flowable<T> {
+    return observeEventsOnUi().ofType()
+}
