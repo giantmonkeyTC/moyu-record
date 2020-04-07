3e59c25a9d694d1a66078f104f5787dcfaa9d43f
gigakuma <qiang.l.x@gmail.com>
2020-04-07 17:57:45 +0800

    structure & collection support rxjava

--- app/src/main/java/cn/troph/tomon/core/collections/BaseCollection.kt
+++ app/src/main/java/cn/troph/tomon/core/collections/BaseCollection.kt
@@ -3,12 +3,25 @@ package cn.troph.tomon.core.collections
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.structures.Base
 import cn.troph.tomon.core.utils.Collection
-import kotlin.reflect.KType
-import kotlin.reflect.typeOf
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
 
+enum class EventType {
+    ADD,
+    REMOVE
+}
+
+data class Event<T>(val type: EventType, val obj: T)
 
 open class BaseCollection<T : Base>(val client: Client, m: Map<String, T>? = null) :
-    Collection<T>(m) {
+    Collection<T>(m), ObservableOnSubscribe<Event<T>> {
+
+    private var emitter: ObservableEmitter<Event<T>>? = null
+
+    override fun subscribe(emitter: ObservableEmitter<Event<T>>?) {
+        this.emitter = emitter
+    }
+
     open fun add(data: Map<String, Any>, identify: ((d: Map<String, Any>) -> String)? = null): T? {
         val id = (if (identify != null) {
             identify(data)
@@ -18,16 +31,24 @@ open class BaseCollection<T : Base>(val client: Client, m: Map<String, T>? = nul
             ?: return null
         val existing = get(id)
         if (existing != null) {
-            existing.patch(data)
+            existing.update(data)
             return existing
         }
         val entry = instantiate(data);
         if (entry != null) {
             put(id, entry)
+            emitter?.onNext(Event(EventType.ADD, entry))
         }
         return entry
     }
 
+    override fun remove(key: String): T? {
+        val entry = super.remove(key)
+        if (entry != null) {
+            emitter?.onNext(Event(EventType.REMOVE, entry))
+        }
+        return entry
+    }
 
     open fun resolve(idOrInstance: Any): Any? {
         if (this.values.contains(idOrInstance))
--- app/src/main/java/cn/troph/tomon/core/structures/Base.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/Base.kt
@@ -2,18 +2,31 @@ package cn.troph.tomon.core.structures
 
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
 
-open class Base(val client: Client, private var data: JsonData = mapOf()) {
+open class Base(val client: Client, private var data: JsonData = mapOf()) :
+    ObservableOnSubscribe<Any> {
+
+    private var emitter: ObservableEmitter<Any>? = null
+
     init {
-        patch(this.data)
+        patch(data)
     }
 
-    val raw get() = this.data
+    val raw get() = data
 
-    open fun patch(data: JsonData) {
-        if (data != null) {
-            this.data += data;
-        }
+    override fun subscribe(emitter: ObservableEmitter<Any>?) {
+        this.emitter = emitter
+    }
+
+    fun update(data: JsonData) {
+        patch(data)
+        emitter?.onNext(this)
+    }
+
+    protected open fun patch(data: JsonData) {
+        this.data += data
     }
 
 }
