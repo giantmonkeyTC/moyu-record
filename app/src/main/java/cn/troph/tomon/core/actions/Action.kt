b1a1edd7074a48dcd2f7b179065a832fe1813f88
gigakuma <qiang.l.x@gmail.com>
2020-04-09 03:56:37 +0800

    actions refine

--- app/src/main/java/cn/troph/tomon/core/actions/Action.kt
+++ app/src/main/java/cn/troph/tomon/core/actions/Action.kt
@@ -3,7 +3,7 @@ package cn.troph.tomon.core.actions
 import cn.troph.tomon.core.Client
 
 abstract class Action<T>(protected val client: Client) {
-    open fun handle(data: Any, extra: Any? = null): T? {
+    open fun handle(data: Any?, extra: Any? = null): T? {
         return null
     }
 }
--- app/src/main/java/cn/troph/tomon/core/actions/ActionManager.kt
+++ app/src/main/java/cn/troph/tomon/core/actions/ActionManager.kt
@@ -1,11 +1,21 @@
 package cn.troph.tomon.core.actions
 
 import cn.troph.tomon.core.Client
+import cn.troph.tomon.core.JsonArray
+import cn.troph.tomon.core.JsonData
+import cn.troph.tomon.core.structures.Guild
 import cn.troph.tomon.core.structures.User
 
 class ActionManager(val client: Client) {
 
-    fun userLogin(data: Any): User? = UserLoginAction(client).handle(data)
-    fun userUpdate(data: Any): User? = UserUpdateAction(client).handle(data)
+    fun userLogin(data: JsonData): User? = UserLoginAction(client).handle(data)
+    fun userLogout(): Unit? = UserLogoutAction(client).handle(null)
+    fun userUpdate(data: JsonData): User? = UserUpdateAction(client).handle(data)
+
+    fun guildFetch(data: JsonArray): List<Guild>? = GuildFetchAction(client).handle(data)
+    fun guildCreate(data: JsonData): Guild? = GuildCreateAction(client).handle(data)
+    fun guildDelete(data: JsonData): Guild? = GuildDeleteAction(client).handle(data)
+    fun guildUpdate(data: JsonData): Guild? = GuildUpdateAction(client).handle(data)
+
 
 }
