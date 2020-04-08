42432b56218f9712435c402c77ae4ee87d5ecb04
gigakuma <qiang.l.x@gmail.com>
2020-04-08 12:13:14 +0800

    restful apis reactive

--- app/src/main/java/cn/troph/tomon/core/network/services/MessageService.kt
+++ app/src/main/java/cn/troph/tomon/core/network/services/MessageService.kt
@@ -2,6 +2,7 @@ package cn.troph.tomon.core.network.services
 
 import cn.troph.tomon.core.JsonData
 import com.google.gson.JsonArray
+import io.reactivex.rxjava3.core.Observable
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -10,51 +11,50 @@ import retrofit2.http.*
 
 interface MessageService {
     @GET("channels/{channelId}/messages")
-    suspend fun getMessages(@Path("channelId") channelId: String, @Header("Authorization") token: String): JsonArray
+    fun getMessages(@Path("channelId") channelId: String, @Header("Authorization") token: String): Observable<JsonArray>
 
-    data class createMessageRequest(
+    data class CreateMessageRequest(
         var content: String
     )
 
     @POST("channels/{channelId}/messages")
     fun createMessage(
-        @Path("channelID") channelId: String, @Body request: createMessageRequest, @Header(
+        @Path("channelID") channelId: String, @Body request: CreateMessageRequest, @Header(
             "Authorization"
         ) token: String
-    ): JsonData
+    ): Observable<JsonData>
 
     @Multipart
     @POST("channels/{channelId}/messages")
-    suspend fun uploadAttachments(
+    fun uploadAttachments(
         @Path("channelID") channelId: String, @PartMap partMap: Map<String, RequestBody>, @Part vararg files: MultipartBody.Part, @Header(
             "Authorization"
         ) token: String
-    ): JsonData
+    ): Observable<JsonData>
 
     @DELETE("channels/{channelId}/messages/{messageId}")
-    suspend fun deleteMessage(
+    fun deleteMessage(
         @Path("channelId") channelId: String, @Path("messageId") messageId: String, @Header(
             "Authorization"
         ) token: String
-    ): Void
+    ): Observable<Void>
 
-    data class updateMessageRequest(
+    data class UpdateMessageRequest(
         val content: String
     )
 
     @PATCH("channels/{channelId}/messages/{messageId}")
-    suspend fun updateMessage(
-        @Path("channelId") channelId: String, @Path("messageId") messageId: String, @Body request: updateMessageRequest, @Header(
+    fun updateMessage(
+        @Path("channelId") channelId: String, @Path("messageId") messageId: String, @Body request: UpdateMessageRequest, @Header(
             "Authorization"
         ) token: String
-    ): JsonArray
+    ): Observable<JsonArray>
 
     @PUT("channels/{channelId}/messages/{messageId}/reactions/{identifier}/@me")
-    suspend fun createReaction(
+    fun addReaction(
         @Path("channelId") channelId: String, @Path("messageId") messageId: String, @Path("identifier") identifier: String, @Header(
             "Authorization"
         ) token: String
-    ): JsonArray
-
+    ): Observable<JsonArray>
 
 }
