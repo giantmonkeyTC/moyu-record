81f5ca167f8df7c88fa77daf56aec7442f9c7fdf
gigakuma <qiang.l.x@gmail.com>
2020-04-09 02:39:59 +0800

    guild structure

--- app/src/main/java/cn/troph/tomon/core/structures/Guild.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/Guild.kt
@@ -2,6 +2,7 @@ package cn.troph.tomon.core.structures
 
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
+import cn.troph.tomon.core.MessageNotificationsType
 import cn.troph.tomon.core.collections.GuildChannelCollection
 import cn.troph.tomon.core.collections.GuildMemberCollection
 import cn.troph.tomon.core.collections.RoleCollection
@@ -9,26 +10,32 @@ import java.time.LocalDateTime
 
 class Guild(client: Client, data: JsonData) : Base(client, data) {
     var id: String = ""
+        private set
     var name: String = ""
+        private set
     var icon: String? = null
+        private set
     var iconURL: String? = null
+        private set
     var position: Int = 0
+        private set
     var joinedAt: LocalDateTime = LocalDateTime.now()
+        private set
     var ownerId: String = ""
+        private set
     var systemChannelId: String? = null
+        private set
     var systemChannelFlags: Int = 0
-    private var _channels: GuildChannelCollection = GuildChannelCollection(this)
-    private var _members: GuildMemberCollection = GuildMemberCollection(this)
-    private var _roles: RoleCollection = RoleCollection(this.client, guildId = this.id)
+        private set
+    var defaultMessageNotifications: MessageNotificationsType =
+        MessageNotificationsType.ONLY_MENTION
+        private set
 
-
-    val channels get() = _channels
-    val members get() = _members
-    val roles get() = _roles
+    val channels: GuildChannelCollection = GuildChannelCollection(this)
+    val members: GuildMemberCollection = GuildMemberCollection(this)
+    val roles: RoleCollection = RoleCollection(this.client, guildId = id)
 
     //TODO LEAVE, UPDATE, ETC.
-
-    val guilds = RoleCollection(client, null, id)
 
     override fun patch(data: JsonData) {
         super.patch(data)
@@ -41,17 +48,17 @@ class Guild(client: Client, data: JsonData) : Base(client, data) {
         if (data.contains("icon")) {
             icon = data["icon"] as String?
         }
-        if (data.contains("iconURL")) {
+        if (data.contains("icon_url")) {
             iconURL = data["iconURL"] as String?
         }
         if (data.contains("position")) {
             position = data["position"] as Int
         }
-        if (data.contains("joinedAt")) {
+        if (data.contains("joined_at")) {
             val date = data["joinedAt"] as String
             joinedAt = LocalDateTime.parse(date)
         }
-        if (data.contains("ownerId")) {
+        if (data.contains("owner_id")) {
             ownerId = data["ownerId"] as String
         }
         if (data.contains("system_channel_id")) {
@@ -59,6 +66,10 @@ class Guild(client: Client, data: JsonData) : Base(client, data) {
         }
         if (data.contains("system_channel_flags")) {
             systemChannelFlags = data["system_channel_flags"] as Int
+        }
+        if (data.contains("default_message_notifications")) {
+            val value = data["default_message_notifications"] as Int
+            defaultMessageNotifications = MessageNotificationsType.fromInt(value)
         }
     }
 }
