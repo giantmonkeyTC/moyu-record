7d3936c9d3e7d4d68f0bcc4d44f913f0e2948d7d
gigakuma <qiang.l.x@gmail.com>
2020-04-10 01:17:40 +0800

    guild member

--- app/src/main/java/cn/troph/tomon/core/structures/GuildMember.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/GuildMember.kt
@@ -8,43 +8,42 @@ import java.time.Instant
 import java.util.*
 
 class GuildMember(client: Client, data: JsonData, val guild: Guild) : Base(client, data) {
+
+    var id: String = ""
+        private set
     var nick: String? = null
+        private set
     var joinedAt: Date? = null
-    var user: User? = null
-    var rawRoles: List<*>? = null
+        private set
+    var rawRoles: List<String> = listOf()
+        private set
 
     override fun patch(data: JsonData) {
         super.patch(data)
-        if (data.containsKey("nick")) {
+        if (data.contains("nick")) {
             nick = data["nick"] as String;
         }
-        if (data.containsKey("joined_at")) {
+        if (data.contains("joined_at")) {
             joinedAt = Date.from(Instant.parse(data["joined_at"] as String))
         }
-        if (data.containsKey("user")) {
-            user = client.users.add(data["user"] as Map<String, Any>);
+        if (data.contains("user")) {
+            val user = client.users.add(data["user"] as JsonData);
+            id = user?.id ?: ""
         }
-        if (data.containsKey("roles")) {
-            rawRoles = data["roles"] as List<*>?;
+        if (data.contains("roles")) {
+            rawRoles = data["roles"] as? List<String> ?: listOf();
         }
     }
 
-    val id get() :String = user!!.id
+    val user get() = client.users.get(id)
 
     val roles get() : GuildMemberRoleCollection = GuildMemberRoleCollection(this)
 
-    val displayName get() = if (nick == null || nick == "") user!!.name else nick
+    val displayName get() = (if (nick == null || nick == "") user?.name else nick) ?: ""
 
     val isOwner get() = id == guild.ownerId
 
     fun hasRole(role: Role): Boolean {
-        val roleId = guild.roles.resolveId(role) { it.id }
-        if (roleId == guild.id)
-            return true
-        return rawRoles!!.indexOf(roleId) != -1
-    }
-
-    override fun toString(): String {
-        return "[CoreGuildMember ${user?.id}] { guild: ${guild.id} }"
+        return if (role.isEveryone) true else rawRoles.indexOf(role.id) != -1
     }
 }
