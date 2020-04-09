2f0fdc7e185ed06408c8f217f6f838359b072ea3
gigakuma <qiang.l.x@gmail.com>
2020-04-10 01:19:06 +0800

    role

--- app/src/main/java/cn/troph/tomon/core/structures/Role.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/Role.kt
@@ -1,21 +1,27 @@
 package cn.troph.tomon.core.structures
 
-import android.nfc.FormatException
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
 
 class Role(client: Client, data: JsonData) : Base(client, data) {
+
     var id: String = ""
+        private set
     var guildId: String = ""
+        private set
     var name: String = ""
+        private set
     var color: Int = 0
+        private set
     var position: Int = 0
+        private set
     var hoist: Boolean = false
+        private set
     var permissions: Permissions = Permissions(0)
+        private set
     var mentionable: Boolean = true
+        private set
 
-
-    // TODO DELETE, UPDATE, ETC.
     override fun patch(data: JsonData) {
         super.patch(data)
         if (data.contains("id")) {
@@ -44,34 +50,27 @@ class Role(client: Client, data: JsonData) : Base(client, data) {
         }
     }
 
-    val index
-        get() : Int {
-            return guild!!.roles.list().indexOf(this)
-        }
-
     val guild get() = client.guilds.get(guildId)
 
     val isEveryone get() = id == guild?.id
 
-    fun comparePositionTo(role: Any): Int {
-        var roleInstance = this.guild?.roles?.resolve(role)
-        if (roleInstance == null)
-            return throw FormatException("Role nor a Snowflake")
-        return comparePositions(this, roleInstance as Role)
+    val index get() = guild!!.roles.list().indexOf(this)
 
+    // TODO DELETE, UPDATE, ETC.
+
+    fun comparePositionTo(role: Role): Int {
+        return comparePositions(this, role)
     }
 
-    fun comparePositions(role1: Role, role2: Role): Int {
-        if (role1.isEveryone != role2.isEveryone)
-            return (if (role1.isEveryone) 1 else 0) - (if (role1.isEveryone) 1 else 0)
-        if (role1.position != role2.position)
-            return role2.position - role1.position
-        return role1.id.compareTo(role2.id)
+    companion object {
+        fun comparePositions(role1: Role, role2: Role): Int {
+            if (role1.isEveryone != role2.isEveryone)
+                return (if (role1.isEveryone) 1 else 0) - (if (role1.isEveryone) 1 else 0)
+            if (role1.position != role2.position)
+                return role2.position - role1.position
+            // TODO string aligned
+            return role1.id.compareTo(role2.id)
+        }
     }
-
-    override fun toString(): String {
-        return "[CoreRole $id] { name: $name }"
-    }
-
 
 }
