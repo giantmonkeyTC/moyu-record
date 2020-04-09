766ba6d4d3fca303d0c8de67f7d28f81b8202062
gigakuma <qiang.l.x@gmail.com>
2020-04-10 02:54:05 +0800

    attachment

--- app/src/main/java/cn/troph/tomon/core/structures/MessageAttachment.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/MessageAttachment.kt
@@ -4,36 +4,40 @@ import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
 
 class MessageAttachment(client: Client, data: JsonData) : Base(client, data) {
+
     var id: String = ""
-    var filename: String = ""
+        private set
+    var fileName: String = ""
+        private set
     var url: String = ""
+        private set
     var size: Int = 0
+        private set
     var width: Int = 0
+        private set
     var height: Int = 0
+        private set
 
     override fun patch(data: JsonData) {
         super.patch(data)
-        if (data.containsKey("id")) {
+        if (data.contains("id")) {
             id = data["id"] as String
         }
-        if (data.containsKey("filename")) {
-            filename = data["filename"] as String
+        if (data.contains("filename")) {
+            fileName = data["filename"] as String
         }
-        if (data.containsKey("size")) {
+        if (data.contains("size")) {
             size = data["size"] as Int
         }
-        if (data.containsKey("url")) {
+        if (data.contains("url")) {
             url = data["url"] as String
         }
-        if (data.containsKey("width")) {
+        if (data.contains("width")) {
             width = data["width"] as Int
         }
-        if (data.containsKey("height")) {
+        if (data.contains("height")) {
             height = data["height"] as Int
         }
     }
 
-    override fun toString(): String {
-        return "[CoreMessageAttachment $id] { filename: $filename }"
-    }
 }
