884950e98d298682eaae5893689b08fb4cb2eb4f
gigakuma <qiang.l.x@gmail.com>
2020-04-10 16:13:43 +0800

    guild channel collection

--- app/src/main/java/cn/troph/tomon/core/collections/GuildChannelCollection.kt
+++ app/src/main/java/cn/troph/tomon/core/collections/GuildChannelCollection.kt
@@ -1,10 +1,13 @@
 package cn.troph.tomon.core.collections
 
+import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
-import cn.troph.tomon.core.structures.Guild
 import cn.troph.tomon.core.structures.GuildChannel
 
-class GuildChannelCollection(val guild: Guild) : BaseCollection<GuildChannel>(guild.client) {
+class GuildChannelCollection(client: Client, private val guildId: String) :
+    BaseCollection<GuildChannel>(client) {
+
+    val guild get() = client.guilds.get(guildId)
 
     // forbid add
     override fun add(
