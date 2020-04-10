1cf086a83c35570f9fcb54048474526a7e47c2bf
gigakuma <qiang.l.x@gmail.com>
2020-04-11 03:49:01 +0800

    voice channel

--- app/src/main/java/cn/troph/tomon/core/structures/VoiceChannel.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/VoiceChannel.kt
@@ -3,11 +3,7 @@ package cn.troph.tomon.core.structures
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.JsonData
 
-class VoiceChannel(client: Client, data: JsonData, guild: Guild) :
-    GuildChannel(client, data, guild) {
+class VoiceChannel(client: Client, data: JsonData) :
+    GuildChannel(client, data) {
 
-
-    override fun toString(): String {
-        return "[CoreVoiceChannal $id] { name: $name }"
-    }
 }
