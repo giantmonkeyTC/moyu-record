05e9093bfd4ba04535ad2cc619346334adbe3ffc
gigakuma <qiang.l.x@gmail.com>
2020-04-16 01:22:51 +0800

    identity wip

--- app/src/main/java/cn/troph/tomon/core/network/socket/handlers/Identity.kt
+++ app/src/main/java/cn/troph/tomon/core/network/socket/handlers/Identity.kt
@@ -2,16 +2,30 @@ package cn.troph.tomon.core.network.socket.handlers
 
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.network.socket.Handler
+import cn.troph.tomon.core.utils.optString
 import com.google.gson.JsonElement
+import com.google.gson.JsonNull
 
 val handleIdentity: Handler = { client: Client, packet: JsonElement ->
-//    val data = packet["d"] as JsonData
-//
-//    (data["guilds"] as JsonArray).forEach {guild ->
-//        if (guild["system_channel_id"] == "0") {
-//            guild["system_channel_id"] = null
-//        }
-//    }
+    val data = packet.asJsonObject["d"].asJsonObject
+    data["guilds"].asJsonArray.forEach { ele ->
+        val guild = ele.asJsonObject
+        if (guild["system_channel_id"].optString == "0") {
+            guild.add("system_channel_id", JsonNull.INSTANCE)
+        }
+    }
+    data["dm_channels"].asJsonArray.forEach { ele ->
+        val channel = ele.asJsonObject
+        if (channel["guild_id"].optString == "0") {
+            channel.addProperty("guild_id", "@me")
+        }
+    }
+    client.actions.guildFetch(data["guilds"].asJsonArray, true)
+    client.actions.channelFetch(data["dm_channels"].asJsonArray, true)
+    data["guilds"].asJsonArray.forEach {
+        val guild = it.asJsonObject
+        client.actions.channelFetch(guild["channels"].asJsonArray, true, guild["id"].asString)
+        client.actions.roleFetch(guild["roles"].asJsonArray, true, guild["id"].asString)
 
-    println(packet)
+    }
 }
