b295f60a731959fda517605b4bd9fb1fce31cd8f
gigakuma <qiang.l.x@gmail.com>
2020-04-19 10:50:46 +0800

    voice channel

--- app/src/main/java/cn/troph/tomon/core/structures/VoiceChannel.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/VoiceChannel.kt
@@ -6,4 +6,26 @@ import com.google.gson.JsonObject
 class VoiceChannel(client: Client, data: JsonObject) :
     GuildChannel(client, data) {
 
+    var bitrate: Int = 36800
+        private set
+    var userLimit: Int = 0
+        private set
+
+    init {
+        patchSelf(data)
+    }
+
+    private fun patchSelf(data: JsonObject) {
+        if (data.has("bitrate")) {
+            bitrate = data["bitrate"].asInt
+        }
+        if (data.has("user_limit")) {
+            userLimit = data["user_limit"].asInt
+        }
+    }
+
+    override fun patch(data: JsonObject) {
+        super.patch(data)
+        patchSelf(data)
+    }
 }
