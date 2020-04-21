9c36c7d5ff0e4f5098d730a1044b1de35f61b81a
gigakuma <qiang.l.x@gmail.com>
2020-04-22 02:02:47 +0800

    add login & register activity

---
+++ app/src/main/java/cn/troph/tomon/ui/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package cn.troph.tomon.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.troph.tomon.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
