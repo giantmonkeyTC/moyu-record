c7a43004794a36904e7e3b209140a4f126d1def4
gigakuma <qiang.l.x@gmail.com>
2020-04-22 02:03:01 +0800

    entry activity

---
+++ app/src/main/java/cn/troph/tomon/ui/activities/EntryActivity.kt
@@ -0,0 +1,42 @@
+package cn.troph.tomon.ui.activities
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cn.troph.tomon.R
+import cn.troph.tomon.core.Client
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+
+class EntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry)
+        Client.global
+            .login()
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                println("login")
+            }, {
+                gotoLogin()
+            })
+    }
+
+    private fun gotoLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(
+            intent,
+            ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            ).toBundle()
+        )
+    }
+
+    private fun gotoRegister() {
+        
+    }
+
+}
