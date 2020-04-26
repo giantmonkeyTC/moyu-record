ebcc6419cfb7e3522356033efffc972f2c4f5109
gigakuma <qiang.l.x@gmail.com>
2020-04-26 21:02:51 +0800

    observable variable

---
+++ app/src/main/java/cn/troph/tomon/ui/states/Variable.kt
@@ -0,0 +1,12 @@
+package cn.troph.tomon.ui.states
+
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+
+class Variable<T>(private val defaultValue: T) {
+    var value: T = defaultValue
+        set(value) {
+            field = value
+            observable.onNext(value)
+        }
+    private val observable = BehaviorSubject.createDefault(value)
+}
