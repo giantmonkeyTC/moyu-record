ff5e855f9a2a9cd2bc888924e4d0524e810d2230
gigakuma <qiang.l.x@gmail.com>
2020-04-29 23:37:47 +0800

    channel panel wip

--- app/src/main/java/cn/troph/tomon/ui/chat/fragments/ChannelPanelFragment.kt
+++ app/src/main/java/cn/troph/tomon/ui/chat/fragments/ChannelPanelFragment.kt
@@ -6,8 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import cn.troph.tomon.R
+import cn.troph.tomon.core.Client
+import cn.troph.tomon.core.structures.TextChannelBase
+import cn.troph.tomon.ui.states.AppState
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 class ChannelPanelFragment : Fragment() {
+
+    private var channelId: String? = null
+        set(value) {
+            val changed = field != value
+            field = value
+            if (changed && value != null) {
+                val channel = Client.global.channels[value]
+                if (channel is TextChannelBase) {
+                    channel.messages.fetch().observeOn(AndroidSchedulers.mainThread()).subscribe {
+
+                    }
+                }
+            }
+        }
+
+    init {
+        AppState.global.channelSelection.observable.observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                channelId = it.channelId
+            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
