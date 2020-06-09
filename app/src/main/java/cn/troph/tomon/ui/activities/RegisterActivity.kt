d8bdb44559171c5c529f98758f228b789f3589da
giantmonkeyTC <tcgiantmonkey@gmail.com>
2020-06-09 17:21:15 +0800

    register page router

--- app/src/main/java/cn/troph/tomon/ui/activities/RegisterActivity.kt
+++ app/src/main/java/cn/troph/tomon/ui/activities/RegisterActivity.kt
@@ -1,13 +1,32 @@
 package cn.troph.tomon.ui.activities
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cn.troph.tomon.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        register_to_login.setOnClickListener {
+            gotoLogin()
+        }
+    }
+
+    private fun gotoLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(
+            intent,
+            ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            ).toBundle()
+        )
+        finish()
     }
 }
