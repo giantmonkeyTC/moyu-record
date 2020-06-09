420b4a1bc3e0c61d4db7ea667a32ad8f386fde74
giantmonkeyTC <tcgiantmonkey@gmail.com>
2020-06-09 17:08:20 +0800

    user info page & sign out

--- app/src/main/java/cn/troph/tomon/ui/chat/fragments/UserInfoFragment.kt
+++ app/src/main/java/cn/troph/tomon/ui/chat/fragments/UserInfoFragment.kt
@@ -1,20 +1,31 @@
 package cn.troph.tomon.ui.chat.fragments
 
+import android.app.ActivityOptions
 import android.app.Dialog
+import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import cn.troph.tomon.R
+import cn.troph.tomon.core.Client
+import cn.troph.tomon.core.structures.Me
+import cn.troph.tomon.ui.activities.LoginActivity
+import cn.troph.tomon.ui.chat.viewmodel.UserInfoViewModel
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_user_information.view.*
 
 class UserInfoFragment : BottomSheetDialogFragment() {
 
+    private val mUserInfoVM: UserInfoViewModel by viewModels()
     lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        mUserInfoVM.loadUserInfo()
         val bottomSheet = super.onCreateDialog(savedInstanceState)
         val view = View.inflate(context, R.layout.fragment_user_information, null)
         bottomSheet.setContentView(view)
@@ -24,6 +35,22 @@ class UserInfoFragment : BottomSheetDialogFragment() {
         bottomSheetBehavior = BottomSheetBehavior.from(view.parent as View)
         bottomSheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO)
         extraSpace.minimumHeight = Resources.getSystem().displayMetrics.heightPixels / 2
+
+        val me = mUserInfoVM.getUserInfoLiveData().value
+        if (me != null) {
+            view.profile_layout.user_info_avatar.user = me
+            view.profile_layout.user_info_name.text = me.name
+            view.profile_layout.user_info_email.text = me.email
+            view.profile_layout.user_info_phone.text = me.phone
+            view.profile_layout.user_info_nick.text = me.username
+        }
+
+        view.profile_layout.user_sign_out.setOnClickListener {
+            Client.global.me.logout()
+            gotoLogin()
+        }
+
+
         bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -48,6 +75,19 @@ class UserInfoFragment : BottomSheetDialogFragment() {
         return bottomSheet
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mUserInfoVM.getUserInfoLiveData().observe(viewLifecycleOwner, Observer {
+            it?.let { me ->
+                view.profile_layout.user_info_avatar.user = me
+                view.profile_layout.user_info_name.text = me.name
+                view.profile_layout.user_info_email.text = me.email
+                view.profile_layout.user_info_phone.text = me.phone
+                view.profile_layout.user_info_nick.text = me.username
+            }
+        })
+    }
+
     override fun onStart() {
         super.onStart()
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
@@ -70,5 +110,17 @@ class UserInfoFragment : BottomSheetDialogFragment() {
             requireContext().theme.obtainStyledAttributes(intArrayOf(android.R.attr.actionBarSize))
         val size = array.getDimension(0, 0F).toInt()
         return size
+    }
+
+    private fun gotoLogin() {
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(
+            intent,
+            ActivityOptions.makeCustomAnimation(
+                requireContext(),
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            ).toBundle()
+        )
     }
 }
--- app/src/main/java/cn/troph/tomon/ui/chat/viewmodel/UserInfoViewModel.kt
+++ app/src/main/java/cn/troph/tomon/ui/chat/viewmodel/UserInfoViewModel.kt
@@ -1,4 +1,21 @@
 package cn.troph.tomon.ui.chat.viewmodel
 
-class UserInfoViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import cn.troph.tomon.core.Client
+import cn.troph.tomon.core.structures.Me
+import cn.troph.tomon.ui.chat.messages.notifyObserver
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+
+class UserInfoViewModel :ViewModel(){
+    private val userInfoLiveData = MutableLiveData<Me>()
+    fun getUserInfoLiveData() = userInfoLiveData
+    fun loadUserInfo(){
+        userInfoLiveData.value = Client.global.me
+        Client.global.me.observable.observeOn(AndroidSchedulers.mainThread()).subscribe{
+            userInfoLiveData.value = Client.global.me
+            userInfoLiveData.notifyObserver()
+        }
+    }
+
 }
