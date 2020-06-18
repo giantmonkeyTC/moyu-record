198b351ceb85e736da2790d044ae81a5fa964668
Ya Zhou <3026426198@qq.com>
2020-06-18 18:24:12 +0800

    unread

--- app/src/main/java/cn/troph/tomon/core/structures/DmChannel.kt
+++ app/src/main/java/cn/troph/tomon/core/structures/DmChannel.kt
@@ -3,8 +3,10 @@ package cn.troph.tomon.core.structures
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.collections.MessageCollection
 import cn.troph.tomon.core.utils.Collection
+import cn.troph.tomon.core.utils.optInt
 import cn.troph.tomon.core.utils.optString
 import com.google.gson.JsonObject
+import com.orhanobut.logger.Logger
 import java.time.LocalDateTime
 
 class DmChannel(client: Client, data: JsonObject) : Channel(client, data), TextChannelBase {
@@ -30,12 +32,16 @@ class DmChannel(client: Client, data: JsonObject) : Channel(client, data), TextC
     override val muted get() = getMuted()
 
     private fun patchSelf(data: JsonObject) {
+        Logger.d(data.toString())
         if (data.has("recipients")) {
             val recipients = data["recipients"].asJsonArray
             if (recipients.size() > 0) {
                 val recipient = client.users.add(recipients.get(0).asJsonObject)
                 recipientId = recipient?.id ?: ""
             }
+        }
+        if (data.has("unread_count")) {
+            unReadCount = data["unread_count"].asInt
         }
         if (data.has("last_message_id")) {
             lastMessageId = data["last_message_id"].optString
