7b096551590d69c945c5857a375ac7a52c0e758f
Ya Zhou <3026426198@qq.com>
2020-06-26 18:18:53 +0800

    added sentry.io

--- app/src/main/java/cn/troph/tomon/TomonApplication.kt
+++ app/src/main/java/cn/troph/tomon/TomonApplication.kt
@@ -7,11 +7,14 @@ import cn.troph.tomon.core.Client
 import com.downloader.PRDownloader
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import io.sentry.android.core.SentryAndroid
+import io.sentry.core.Sentry
 
 class TomonApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        SentryAndroid.init(this)
         EmojiCompat.init(BundledEmojiCompatConfig(this))
         Logger.addLogAdapter(AndroidLogAdapter())
         PRDownloader.initialize(this)
