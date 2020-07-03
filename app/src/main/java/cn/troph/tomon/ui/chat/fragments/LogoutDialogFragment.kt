307a447f6c454025f9354fc1753c79cd1843c066
giantmonkeyTC <tcgiantmonkey@gmail.com>
2020-07-03 18:23:05 +0800

    profile redesign

---
+++ app/src/main/java/cn/troph/tomon/ui/chat/fragments/LogoutDialogFragment.kt
@@ -0,0 +1,50 @@
+package cn.troph.tomon.ui.chat.fragments
+
+import android.app.ActivityOptions
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.edit
+import androidx.fragment.app.DialogFragment
+import cn.troph.tomon.R
+import cn.troph.tomon.core.Client
+import cn.troph.tomon.ui.activities.EntryOptionActivity
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.dialog_check_logout.*
+
+class LogoutDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage(R.string.porofile_check_logout)
+                .setPositiveButton(R.string.profile_confirm_logout,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        gotoEntryOption()
+                    })
+                .setNegativeButton(R.string.profile_cancel_logout,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        dialog.dismiss()
+                    })
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    private fun gotoEntryOption() {
+        val intent = Intent(requireContext(), EntryOptionActivity::class.java)
+        startActivity(
+            intent,
+            ActivityOptions.makeCustomAnimation(
+                requireContext(),
+                R.animator.top_to_bottom_anim,
+                R.animator.top_to_bottom_anim
+            ).toBundle()
+        )
+        requireActivity().finish()
+    }
+}
