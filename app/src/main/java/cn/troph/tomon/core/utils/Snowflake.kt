8d4f8a418bb3e1dd8dd015487855de9cbfa2968c
giantmonkeyTC <tcgiantmonkey@gmail.com>
2020-07-06 14:09:26 +0800

    default avatar update

--- app/src/main/java/cn/troph/tomon/core/utils/Snowflake.kt
+++ app/src/main/java/cn/troph/tomon/core/utils/Snowflake.kt
@@ -1,19 +1,52 @@
 package cn.troph.tomon.core.utils
 
+import com.airbnb.lottie.parser.IntegerParser
+import com.alibaba.sdk.android.oss.common.utils.BinaryUtil
+import com.google.gson.annotations.SerializedName
+import java.sql.Timestamp
+
 class Snowflake(val value: String = "") : Comparable<Snowflake> {
     companion object {
-//        fun deconstruct(snowflake: Snowflake) {
-//            val binary =
-//
-//        }
-//
-//        fun idToBinary(num: String) {
-//            var bin =
+        const val EPOCH: Long = 1559347200000
+
+        data class res(
+            @SerializedName("timestamp") val timestamp: Long,
+            @SerializedName("workerId") val workerId: Int,
+            @SerializedName("processId") val processId: Int,
+            @SerializedName("increment") val increment: Int,
+            @SerializedName("binary") val binary: String
+        )
+
+        fun deconstruct(snowflake: String): res {
+            val binary = idToBinary(snowflake).padStart(64, '0')
+            println(binary)
+            return res(
+                timestamp = binary.substring(0, 42).toLong(2) + EPOCH,
+                workerId = binary.substring(42, 47).toInt(2),
+                processId = binary.substring(47, 52).toInt(2),
+                increment = binary.substring(52, 64).toInt(2),
+                binary = binary
+            )
+        }
+
+        fun idToBinary(num: String): String {
+            val long = num.toLong()
+            val binary = long.toString(2)
+//            var bin = ""
 //            var high =
 //                num.slice(IntRange(0, -10))
 //                    .toInt()
 //            var low = num.slice()
-//        }
+//            while (low > 0 || high > 0) {
+//                bin = low + bin
+//                low = Math.floor(low / 2)
+//                if (high > 0) {
+//                    low = low + 5000000000 * (high % 2)
+//                    high = Math.floor((high / 2).toDouble()).toInt()
+//                }
+//            }
+            return binary
+        }
     }
 
     val aligned: String
