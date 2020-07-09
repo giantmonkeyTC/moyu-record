bfe688b8dec89ed0ac2e07927180ee2e4173fa9d
Ya Zhou <3026426198@qq.com>
2020-07-09 14:54:45 +0800

    fix

--- app/src/main/java/cn/troph/tomon/core/network/Restful.kt
+++ app/src/main/java/cn/troph/tomon/core/network/Restful.kt
@@ -6,13 +6,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class Restful {
 
     private val client: OkHttpClient by lazy {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
-        OkHttpClient.Builder().addInterceptor(interceptor).build()
+        OkHttpClient.Builder().addInterceptor(interceptor).callTimeout(5,TimeUnit.MINUTES).build()
     }
 
     private val retrofit = Retrofit
