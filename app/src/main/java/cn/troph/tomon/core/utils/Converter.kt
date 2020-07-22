9f3ceb6702fe9d08b1f334298d7e65bb6c448ac8
incomingflyingbrick <3026426198@qq.con>
2020-07-22 16:44:21 +0800

    fix time

--- app/src/main/java/cn/troph/tomon/core/utils/Converter.kt
+++ app/src/main/java/cn/troph/tomon/core/utils/Converter.kt
@@ -2,6 +2,7 @@ package cn.troph.tomon.core.utils
 
 import com.google.gson.JsonPrimitive
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
@@ -32,13 +33,23 @@ object Converter {
         return when (value) {
             is String -> {
                 if (value.isNotEmpty()) {
-                    LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME).plusHours(8)
+
+                    LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME)
+                        .atZone(ZoneId.of("GMT+0")).withZoneSameInstant(
+                        ZoneId.systemDefault()
+                    ).toLocalDateTime()
                 } else {
                     LocalDateTime.now()
                 }
             }
-            is JsonPrimitive -> LocalDateTime.parse(value.asString,DateTimeFormatter.ISO_DATE_TIME).plusHours(8)
-            else -> LocalDateTime.parse(value.toString(),DateTimeFormatter.ISO_DATE_TIME).plusHours(8)
+            is JsonPrimitive -> LocalDateTime.parse(value.asString, DateTimeFormatter.ISO_DATE_TIME)
+                .atZone(ZoneId.of("GMT+0")).withZoneSameInstant(
+                ZoneId.systemDefault()
+            ).toLocalDateTime()
+            else -> LocalDateTime.parse(value.toString(), DateTimeFormatter.ISO_DATE_TIME)
+                .atZone(ZoneId.of("GMT+0")).withZoneSameInstant(
+                ZoneId.systemDefault()
+            ).toLocalDateTime()
         }
     }
 }
