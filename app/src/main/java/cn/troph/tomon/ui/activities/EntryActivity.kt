1d65cdaf7f8f3fcbe07a847e28e6e63c78fa668c
incomingflyingbrick <3026426198@qq.con>
2020-07-23 17:44:08 +0800

    fixed keys

--- app/src/main/java/cn/troph/tomon/ui/activities/EntryActivity.kt
+++ app/src/main/java/cn/troph/tomon/ui/activities/EntryActivity.kt
@@ -4,23 +4,12 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import cn.troph.tomon.R
 import cn.troph.tomon.core.Client
-import cn.troph.tomon.core.events.GuildFetchEvent
-import cn.troph.tomon.core.events.GuildSyncEvent
-import cn.troph.tomon.core.utils.event.observeEventOnUi
 import cn.troph.tomon.ui.chat.viewmodel.DataPullingViewModel
-import com.orhanobut.logger.Logger
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.functions.Consumer
-import java.util.concurrent.TimeUnit
-
+//cn.troph.tomon.ui.activities.EntryActivity
 class EntryActivity : BaseActivity() {
 
 
