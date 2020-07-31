a5e75e107a8c29dc909fcd0b99a447b30ed31dd3
incomingflyingbrick <3026426198@qq.con>
2020-07-31 10:52:28 +0800

    added dialog for agreement

--- app/src/main/java/cn/troph/tomon/ui/activities/RegisterActivity.kt
+++ app/src/main/java/cn/troph/tomon/ui/activities/RegisterActivity.kt
@@ -3,10 +3,21 @@ package cn.troph.tomon.ui.activities
 import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
-import android.inputmethodservice.Keyboard
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import cn.troph.tomon.R
 import cn.troph.tomon.core.Client
 import cn.troph.tomon.core.network.services.AuthService
@@ -16,6 +27,7 @@ import com.google.android.material.snackbar.Snackbar
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register.*
+
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -75,7 +87,42 @@ class RegisterActivity : AppCompatActivity() {
                     ).show()
                 }, {})
         }
+
+        val ss = SpannableString(getString(R.string.privacy_agreement))
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val alert: AlertDialog.Builder = AlertDialog.Builder(this@RegisterActivity)
+                alert.setTitle("TOMON")
+                val wv = WebView(this@RegisterActivity)
+                wv.loadUrl("https://beta.tomon.co")
+                wv.webViewClient = object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(
+                        view: WebView,
+                        url: String
+                    ): Boolean {
+                        view.loadUrl(url)
+                        return true
+                    }
+                }
+
+                alert.setView(wv)
+                alert.setNegativeButton("关闭", { dialog, id -> dialog.dismiss() })
+                alert.show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+
+        ss.setSpan(clickableSpan, 5, 13, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+
+        private_agreement_tv.text = ss
+        private_agreement_tv.movementMethod = LinkMovementMethod.getInstance()
+        private_agreement_tv.highlightColor = Color.TRANSPARENT
     }
+
 
     private fun gotoEntryOption() {
         val intent = Intent(this, EntryOptionActivity::class.java)
