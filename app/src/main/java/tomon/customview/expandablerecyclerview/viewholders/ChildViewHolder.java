b274ebf3d991e9fc29aea437bc1f81f8710c7277
Owl <lumiahuan@gmail.com>
2020-08-28 11:07:27 +0800

    import expandable recycler view and compat androidx
    Signed-off-by: Owl <lumiahuan@gmail.com>

---
+++ app/src/main/java/tomon/customview/expandablerecyclerview/viewholders/ChildViewHolder.java
@@ -0,0 +1,15 @@
+package tomon.customview.expandablerecyclerview.viewholders;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import tomon.customview.expandablerecyclerview.models.ExpandableGroup;
+
+/**
+ * ViewHolder for {@link ExpandableGroup#items}
+ */
+public class ChildViewHolder extends RecyclerView.ViewHolder {
+
+  public ChildViewHolder(View itemView) {
+    super(itemView);
+  }
+}
